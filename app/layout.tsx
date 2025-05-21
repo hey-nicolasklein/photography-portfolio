e59b7993d4ec08487826e98de750142dc7233500
Nicolas Klein <nicolasklein1998@gmail.com>
2025-05-21 22:10:01 +0200

    Added vercel statistics

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ScrollProgress from "@/components/scroll-progress";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -33,6 +34,7 @@ export default function RootLayout({
             <body className={`${inter.variable} font-sans antialiased`}>
                 <ScrollProgress />
                 {children}
+                <SpeedInsights />
             </body>
         </html>
     );
