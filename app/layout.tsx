cec216c045f5305fed125803df90527e68d2e2b7
Nicolas Klein <32168266+hey-nicolasklein@users.noreply.github.com>
2025-10-22 14:59:55 +0000

    Bugfix: Globals setting legacy gradient visible in overscroll background

--- app/layout.tsx
+++ app/layout.tsx
@@ -128,7 +128,7 @@ export default function RootLayout({
                     content="/browserconfig.xml"
                 />
             </head>
-            <body className={`${inter.variable} font-sans antialiased bg-white`}>
+            <body className={`${inter.variable} font-sans antialiased`}>
                 <script
                     type="application/ld+json"
                     dangerouslySetInnerHTML={{
